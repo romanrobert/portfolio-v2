b36eeae5d5cd950d69bbc8492a1554723d59ae32
romanrobert <iromanrobert@gmail.com>
2025-02-18 10:22:38 +0200

    changed mobile menu animation

--- src/components/ui/sidebar/anim.ts
+++ src/components/ui/sidebar/anim.ts
@@ -1,15 +1,15 @@
 export const menuSlide = {
   initial: {
-    x: "100%",
+    y: "-100%",
     opacity: 0,
   },
   enter: {
-    x: "0%",
+    y: "0%",
     opacity: 1,
     transition: { duration: 0.5, ease: [0.46, 0.03, 0.52, 0.96] },
   },
   exit: {
-    x: "100%",
+    y: "-100%",
     opacity: 0,
     transition: { duration: 0.5, ease: [0.46, 0.03, 0.52, 0.96] },
   },
@@ -17,13 +17,13 @@ export const menuSlide = {
 
 export const slide = {
   initial: {
-    x: "80px",
+    y: "80px",
   },
   enter: {
-    x: "0px",
+    y: "0px",
     transition: { duration: 0.5, ease: [0.46, 0.03, 0.52, 0.96] },
   },
   exit: {
-    x: "80px",
+    y: "80px",
   },
 };
